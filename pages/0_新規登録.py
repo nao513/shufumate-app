ec4ac4bd1fc7b0ab3b36afa0b739ca0700964a1d
nao513 <ogenkidesuka513@gmail.com>
2026-04-19 11:58:09 +0900

    Update 0_新規登録.py

--- pages/0_新規登録.py
+++ pages/0_新規登録.py
@@ -11,17 +11,37 @@ if is_logged_in():
         st.switch_page("Home.py")
     st.stop()
 
+current_year = date.today().year
+
 with st.form("register_form"):
     login_id = st.text_input("ログインID")
     password = st.text_input("パスワード", type="password")
     password_confirm = st.text_input("パスワード（確認）", type="password")
     nickname = st.text_input("ニックネーム")
-    birth_date = st.date_input(
-        "生年月日",
-        value=date(1976, 1, 1),
-        min_value=date(1900, 1, 1),
-        max_value=date.today(),
-    )
+
+    st.markdown("**生年月日**")
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        birth_year = st.selectbox(
+            "年",
+            options=list(range(current_year, 1899, -1)),
+            index=list(range(current_year, 1899, -1)).index(1976) if 1976 in list(range(current_year, 1899, -1)) else 0,
+        )
+
+    with col2:
+        birth_month = st.selectbox(
+            "月",
+            options=list(range(1, 13)),
+            index=0,
+        )
+
+    with col3:
+        birth_day = st.selectbox(
+            "日",
+            options=list(range(1, 32)),
+            index=0,
+        )
 
     submitted = st.form_submit_button("登録する", use_container_width=True)
 
@@ -30,6 +50,12 @@ if submitted:
         if password != password_confirm:
             st.error("パスワード確認が一致しません")
         else:
+            try:
+                birth_date = date(int(birth_year), int(birth_month), int(birth_day))
+            except ValueError:
+                st.error("生年月日が正しくありません")
+                st.stop()
+
             user_record = create_user(
                 login_id=login_id,
                 password=password,
